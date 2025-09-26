7f8518db9a12510beb3c437bc1b78b74f08f7889
harsh-accio <rajputharsh9897@gmail.com>
2025-09-26 10:26:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here!
+const ol = document.getElementById('infi-list')
+let totalitems=0;
+function listItem(count){
+	for(let i=0; i<count; i++){
+		totalitems++;
+		let li = document.createElement('li');
+		li.innerText=`Item ${totalitems}`;
+		ol.appendChild(li);
+	}
+}
+listItem(10);
 
+ol.addEventListener('scroll', () => {
+  if (ol.scrollTop + ol.clientHeight >= ol.scrollHeight - 1) {
+    listItem(2);
+  }
+});
